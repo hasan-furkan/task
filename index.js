a5873f746c1bf9311507f1742f10dae563e1a7c8
hasanfurkan <hsnfrkn32@gmail.com>
2022-04-13 03:48:43 +0300

    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+
+
+
+btn1.onclick = function(){
+   
+btn2.style.backgroundColor = 'white';
+btn2.style.color = "#232B76"
+btn1.style.backgroundColor= "#232B76";
+    btn1.style.color= "white"
+}
+
+btn2.onclick = function(){
+   
+    btn1.style.backgroundColor = 'white';
+    btn1.style.color = "#232B76";
+    btn2.style.backgroundColor= "#232B76";
+    btn2.style.color= "white"
+}
+
